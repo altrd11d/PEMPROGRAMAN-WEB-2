cc278faf29916d9f69cbde3069e642f8dfa15545
altrd11d <160314476+altrd11d@users.noreply.github.com>
2026-05-01 10:04:30 +0700

    Update greeting names in sapa function calls

--- p9/p9_sapa.php
+++ p9/p9_sapa.php
@@ -3,8 +3,8 @@ function sapa($nama) {
     return "Halo, " . $nama . "!";
 }
 
-echo sapa("Surya"); // Output: Halo, Surya!
+echo sapa("Alfitri"); // Output: Halo, Alfitri!
 echo "<br />";
-echo sapa("Maya"); // Output: Halo, Maya!
-// Project 9.1 By ASEP SURYA AGUSTIN - 221011450392
+echo sapa("Deviani"); // Output: Halo, Deviani!
+
 ?>
