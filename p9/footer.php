c8f41f4f83919356cdb868253b9b276f52da9484
altrd11d <160314476+altrd11d@users.noreply.github.com>
2026-05-01 10:00:55 +0700

    Add files via upload

---
+++ p9/footer.php
@@ -0,0 +1,4 @@
+<?php
+echo "Ini adalah footer.<br>";
+// Project 9.8 By ASEP SURYA AGUSTIN - 221011450392
+?>
---
+++ p9/header.php
@@ -0,0 +1,4 @@
+<?php
+echo "Ini adalah header.<br>";
+// Project 9.7 By ASEP SURYA AGUSTIN - 221011450392
+?>
